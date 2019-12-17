e4b05c442df78028d2f92cb2f7397c826871e855
Alex <sanchoskorch765@gmail.com>
2019-12-17 22:35:01 +0200

    Add files via upload

---
+++ App.js
@@ -0,0 +1,8 @@
+import StackNavigator from 'react-navigation';
+
+import {Main,Photo} from './Pages';
+
+export default App = StackNavigator({
+    Main: {screen: Main},
+    Photo: {screen: Photo},
+});
